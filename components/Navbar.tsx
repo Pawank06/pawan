a2d5d57b87249fc4f97fb7e44ae8362efcf1040f
Pawank06 <pawan06kumar2003@gmail.com>
2024-02-07 12:44:18 +0530

    worked on navbar and project heading again 3.0

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
 
     const changeBackground = () => {
-        if (window.scrollY >= 90) {
+        if (window.scrollY >= 0) {
             setNavbar(true)
         } else {
             setNavbar(false)
@@ -24,15 +24,13 @@ const Navbar = () => {
 
     return (
 
-        <div className={navbar ? 'text-rose-100/100 lg:text-lg  md:text-lg text-sm gap-5 font-bold my-5 nav-bar active flex items-center w-full max-w-[320px] md:max-w-2xl lg:max-w-3xl rounded-xl justify-between px-4 py-5' : 'w-full text-md mb-14 lg:text-lg md:text-lg font-semibold'}>
-            {
-                navbar ? <div>
+        <div className='text-rose-100/100 lg:text-lg  md:text-lg text-sm gap-5 lg:font-bold md:font-bold font-semibold my-5 nav-bar active flex items-center w-full max-w-[320px] md:max-w-2xl lg:max-w-3xl rounded-xl justify-between px-4 py-5'>
+            <div>
                 <Link href="/" className='hover:text-rose-100/90 opacity-70 hover:underline'>
                     Logo
                 </Link>
-            </div> : ''
-            }
-            
+            </div>
+
             <div className='flex gap-3 lg:gap-7 md:gap-6'>
                 <div className=''>
                     <Link href="/" className='hover:text-rose-100/90 opacity-70 hover:underline'>
